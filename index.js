32ebe5bed3a3d280bca19220de6b4e1a26cd8280
Ashwanth Fernando <ashwanthfernando@gmail.com>
2024-12-24 20:33:01 +1100

    Remove node_modules from git tracking

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const resolvers = {
         games: () => db.games,  
         authors: () => db.authors,
         reviews: () => db.reviews,
+        review(_, args) {
+            return db.reviews.find(review => review.id === args.id)
+        },
+        game(_, args) {
+            return db.games.find(game => game.id === args.id)
+        },
+        author(_, args) {
+            return db.authors.find(author => author.id === args.id)
+        }
+    },
+    Game: {
+        reviews: (game) => db.reviews.filter(review => review.game_id === game.id)
+    },
+    Author: {
+        reviews: (author) => db.reviews.filter(review => review.author_id === author.id)
+    },
+    Review: {
+        author: (review) => db.authors.find(author => author.id === review.author_id),
+        game: (review) => db.games.find(game => game.id === review.game_id)
     }
 }
 
